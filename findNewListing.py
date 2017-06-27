a689cc8f41274183abb8f57d9b4c181ee8724d9b
Explomatic <morten.madsen.92@gmail.com>
2017-06-27 23:54:00 +0200

    Changed structure of loop through the tags. Implemented method for comparing known flats and found flats in order to search for new ones. Added a "flat" class.

--- findNewListing.py
+++ findNewListing.py
@@ -1,75 +1,48 @@
 from bs4 import BeautifulSoup
 from urllib import request
-import smtplib
 from email.mime.text import MIMEText
-import sys
-import re
 import quickstart as qs
-import pdb
+import sys, os.path, re, jsonpickle, io, pdb
+
+class flat:
+    pass
 
 def fetch_all_listings(url):
     webReq = request.urlopen(url)
     soup = BeautifulSoup(webReq, 'lxml')
     return(soup)
 
-def find_flat_information(soup):
+def get_flat_information(soup):
     tags = soup.find_all('td')
 
-    flat_information=[]
-    list_title = []
-    list_address = []
-    list_rooms = []
-    list_size = []
-    list_rent = []
-    list_utility = []
-    list_date = []
-
-    itr = 0
+    flats={}
+    base_url = "http://findbolig.nu"
+    list_of_flat_objs = []
+    counter = 0
     for tag in tags:
-        try:
-            if tag.a['class'][0] == "advertLink":
-                tagCenter = 0
-                tagRight = 0
-                list_title += [str(tag.a['title'])]
-                list_address += [extract_address(tag)]
-        except Exception as e:
-            pass
+        if "advertLink" in str(tag) and not tag.img:
+            tmpFlat = flat()
+            counter = 1
+            tmpFlat.title = str(tag.a['title'])
+            tmpFlat.address = extract_address(tag)
+            tmpFlat.url = base_url + tag.a['href']
+            tmpFlat.flat_id = extract_flat_id(tmpFlat.url)
+            continue
 
-        try:
-            if "center" in tag['style'] and tagCenter < 2:
-                if tagCenter == 0:
-                    list_rooms += [extract_rooms(tag)]
-                else:
-                    list_size += [extract_size(tag)]
+        if counter == 1:
+            tmpFlat.number_of_rooms = extract_rooms(tag)
+            counter += 1
+        elif counter == 2:
+            tmpFlat.size = extract_size(tag)
+            counter += 1
+        elif counter == 3:
+            tmpFlat.rent, tmpFlat.utility = extract_rent_and_utility(tag)
+            counter += 1
+        elif counter == 4:
+            tmpFlat.date_available = extract_availability_date(tag)
+            flats[tmpFlat.flat_id] = tmpFlat
 
-                tagCenter += 1
-        except Exception as e:
-            pass
-
-        try:
-            if "right" in tag['style'] and tagRight < 2:
-                if tagRight == 0:
-                    rent, utility = extract_rent_and_utility(tag)
-                    #pdb.set_trace()
-                    list_rent += [rent]
-                    list_utility += [utility]
-                else:
-                    list_date += [extract_availability_date(tag)]
-                    #list_date += [available_from]
-
-                tagRight += 1
-        except Exception as e:
-            pass
-
-
-#        if itr >= 35:
-#            break
-
-#        itr += 1
-
-    #pdb.set_trace()
-    #var = 2
-    return flat_information
+    return flats
 
 def extract_address(tag):
     match = re.search(r"<b>([\w\s,.-]+?)<\/b><br\/>([\w\s,.-]+?)<\/a><\/td>", str(tag))
@@ -85,7 +58,7 @@ def extract_rooms(tag):
     number_of_rooms = ""
 
     if match:
-        number_of_rooms = str(match.group(1))
+        number_of_rooms = float(match.group(1))
 
     return number_of_rooms
 
@@ -94,7 +67,7 @@ def extract_size(tag):
     size = ""
 
     if match:
-        size = str(match.group(1))
+        size = float(match.group(1))
 
     return size
 
@@ -118,8 +91,37 @@ def extract_availability_date(tag):
 
     return date
 
-def check_for_new_flats(list_of_flats):
-    new_flats = []
+def extract_flat_id(url):
+    match = re.search(r'aid=([\d]+?)\&amp', url)
+    flat_id = ""
+
+    if match:
+        flat_id = float(match.group(1))
+
+    return flat_id
+
+def check_for_new_flats(found_flats):
+    new_flats = {}
+    filename = 'known_flats.txt'
+
+    if os.path.isfile(filename):
+        with io.open(filename, 'r', encoding="utf-8") as f:
+            content = fid.read()
+            known_flats=jsonpickle.decode(content)
+            first_time = False
+    else:
+        f = io.open('known_flats.txt', 'w', encoding="utf-8")
+        first_time = True
+
+    if first_time:
+        jsonObj = jsonpickle.encode(found_flats)
+        f.write(jsonObj)
+        f.close()
+        new_flats = found_flats
+    else:
+        for key, value in found_flats.items():
+            if key not in known_flats.keys():
+                new_flats[key] = value
 
     return new_flats
 
@@ -137,14 +139,37 @@ def send_mail(mail_content):
     qs.send_message(service_id, "me", msg)
 
 def main():
+    args = sys.argv[1:]
+    # if not args:
+    #     max_rent = 15000
+    #     min_rooms = 2
+    #     min_m2 = 70
+    # else:
+    #     if args[0] == '--todir':
+    #         todir = args[1]
+    #         del args[0:2]
+    #
+    #     tozip = ''
+    #     if args[0] == '--tozip':
+    #         tozip = args[1]
+    #         del args[0:2]
+    #
+    #     if len(args) == 0:
+    #         print
+    #         "error: must specify one or more dirs"
+    #         sys.exit(1)
+
     url = 'https://www.findbolig.nu/ledigeboliger/liste.aspx?sa=83328&where=1000-2400,%202800,%202820,%202840,%202900,%202920&m2min=70&rentmax=15000&roomsmin=2&showrented=1&showyouth=0&showOpenDay=0&showlimitedperiod=0&showunlimitedperiod=1&page=1&pagesize=100'
+
     soup = fetch_all_listings(url)
 
-    flat_information = find_flat_information(soup)
+    flats = get_flat_information(soup)
 
-    new_flats = check_for_new_flats(flat_information)
+    new_flats = check_for_new_flats(flats)
 
-    send_mail(new_flats)
+    if new_flats:
+        #send_mail(new_flats)
+        print("Now we would send a mail!")
 
 if __name__ == "__main__":
     main()
